9b6195a117e470f642cbaa2c8e6a307a33a18242
Berk Acunaş <berkacunas@gmail.com>
2025-04-16 16:39:04 +0300

    Project initialized.

---
+++ mainwindow.cpp
@@ -0,0 +1,13 @@
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    this->setWindowTitle("Alexandria");
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
